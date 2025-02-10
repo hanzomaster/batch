67cdb94854080a9cb5b8f93e48ce20c369ace385
Thinh Tran <thinhrom11@gmail.com>
2025-02-10 09:59:10 +0700

    chore: Minor improvement

--- src/main/java/thinkmath/com/batch/dto/Page.java
+++ src/main/java/thinkmath/com/batch/dto/Page.java
@@ -1,6 +1,3 @@
 package thinkmath.com.batch.dto;
 
-import java.util.List;
-import java.util.Map;
-
 public record Page(int pageNumber, int size) {}
--- src/main/java/thinkmath/com/batch/util/ElasticsearchExecutor.java
+++ src/main/java/thinkmath/com/batch/util/ElasticsearchExecutor.java
@@ -1,6 +1,11 @@
 package thinkmath.com.batch.util;
 
 import co.elastic.clients.elasticsearch.core.SearchResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import thinkmath.com.batch.dto.Page;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -10,10 +15,6 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Component;
-import thinkmath.com.batch.dto.Page;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -22,17 +23,6 @@ public class ElasticsearchExecutor {
     private final ElasticsearchService client;
     private final ExecutorService executorService = Executors.newFixedThreadPool(QueryBuilder.FIXED_THREAD_POOL);
 
-    private static List<Page> paginate(long totalSize, int batchSize) {
-        List<Page> pages = new ArrayList<>();
-        int pageNumber = 0;
-        while (totalSize > 0) {
-            int currentBatchSize = (int) Math.min(batchSize, totalSize);
-            pages.add(new Page(pageNumber++, currentBatchSize));
-            totalSize -= currentBatchSize;
-        }
-        return pages;
-    }
-
     public boolean healthCheck() throws IOException {
         return client.healthCheck();
     }
@@ -40,8 +30,10 @@ public class ElasticsearchExecutor {
     public List<String> executeUseCase(int usecaseNumber) throws IOException {
         SearchResponse<Map> clientQuery =
                 client.query(QueryBuilder.CLIENT_INDEX, QueryBuilder.buildClientQueryUsecase1(), true);
+
         long totalClients = Objects.requireNonNull(clientQuery.hits().total()).value();
         List<Page> clientPaginate = paginate(totalClients, QueryBuilder.BATCH_SIZE);
+
         List<CompletableFuture<List<String>>> allFutures = new ArrayList<>();
         for (Page clientPage : clientPaginate) {
             CompletableFuture<List<String>> future = CompletableFuture.supplyAsync(
@@ -55,7 +47,20 @@ public class ElasticsearchExecutor {
                     executorService);
             allFutures.add(future);
         }
-        CompletableFuture.allOf(allFutures.toArray(new CompletableFuture[0])).join();
-        return null;
+        return allFutures.stream()
+                .map(CompletableFuture::join)
+                .flatMap(List::stream)
+                .toList();
+    }
+
+    private List<Page> paginate(long totalSize, int batchSize) {
+        List<Page> pages = new ArrayList<>();
+        int pageNumber = 0;
+        while (totalSize > 0) {
+            int currentBatchSize = (int) Math.min(batchSize, totalSize);
+            pages.add(new Page(pageNumber++, currentBatchSize));
+            totalSize -= currentBatchSize;
+        }
+        return pages;
     }
 }
